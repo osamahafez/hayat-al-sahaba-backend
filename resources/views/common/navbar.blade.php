fa549a6c30059c5de837a1a6e6099dfbef77b0f3
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2020-01-19 15:57:21 +0200

    chore: clean up

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -31,7 +31,7 @@
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                                {{ __('ar.logout') }}
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
